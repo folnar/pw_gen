5cbe374795934af549f5de4611f5c932efc17a5b
David Casale <dcasale@umd.edu>
2016-01-18 09:40:35 -0500

    2016011801 - initial commit of working solution

---
+++ PwMemoGenerator/frmPWMemoMainMenu.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PwMemoGenerator
+{
+    partial class frmPWMemoMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPWMemoMainMenu));
+            this.btnGenerateNewPWs = new System.Windows.Forms.Button();
+            this.btnCloseAndExit = new System.Windows.Forms.Button();
+            this.btnPrintPWMemos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerateNewPWs
+            // 
+            this.btnGenerateNewPWs.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerateNewPWs.Location = new System.Drawing.Point(12, 12);
+            this.btnGenerateNewPWs.Name = "btnGenerateNewPWs";
+            this.btnGenerateNewPWs.Size = new System.Drawing.Size(179, 78);
+            this.btnGenerateNewPWs.TabIndex = 1;
+            this.btnGenerateNewPWs.Text = "&Generate New Passwords";
+            this.btnGenerateNewPWs.UseVisualStyleBackColor = true;
+            this.btnGenerateNewPWs.Click += new System.EventHandler(this.btnGenerateNewPWs_Click);
+            // 
+            // btnCloseAndExit
+            // 
+            this.btnCloseAndExit.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCloseAndExit.Location = new System.Drawing.Point(382, 12);
+            this.btnCloseAndExit.Name = "btnCloseAndExit";
+            this.btnCloseAndExit.Size = new System.Drawing.Size(179, 78);
+            this.btnCloseAndExit.TabIndex = 3;
+            this.btnCloseAndExit.Text = "E&xit Application";
+            this.btnCloseAndExit.UseVisualStyleBackColor = true;
+            this.btnCloseAndExit.Click += new System.EventHandler(this.btnCloseAndExit_Click);
+            // 
+            // btnPrintPWMemos
+            // 
+            this.btnPrintPWMemos.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrintPWMemos.Location = new System.Drawing.Point(197, 12);
+            this.btnPrintPWMemos.Name = "btnPrintPWMemos";
+            this.btnPrintPWMemos.Size = new System.Drawing.Size(179, 78);
+            this.btnPrintPWMemos.TabIndex = 2;
+            this.btnPrintPWMemos.Text = "&Print Existing Password Memos";
+            this.btnPrintPWMemos.UseVisualStyleBackColor = true;
+            this.btnPrintPWMemos.Click += new System.EventHandler(this.btnPrintPWMemos_Click);
+            // 
+            // frmPWMemoMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 105);
+            this.Controls.Add(this.btnPrintPWMemos);
+            this.Controls.Add(this.btnCloseAndExit);
+            this.Controls.Add(this.btnGenerateNewPWs);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPWMemoMainMenu";
+            this.Text = "Password Memos Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateNewPWs;
+        private System.Windows.Forms.Button btnCloseAndExit;
+        private System.Windows.Forms.Button btnPrintPWMemos;
+    }
+}
+
---
+++ PwMemoGenerator/frmPWMemoMainMenu.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using MySql.Data.MySqlClient;
+
+namespace PwMemoGenerator
+{
+    public partial class frmPWMemoMainMenu : Form
+    {
+        public frmPWMemoMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        Microsoft.Office.Interop.Word.Application app;
+        Document doc;
+        object missing = Type.Missing;
+            
+        private void btnGenerateNewPWs_Click(object sender, EventArgs e)
+        {
+            //killprocess("winword");
+            app = new Microsoft.Office.Interop.Word.Application { Visible = false };
+            
+            System.Data.DataTable dt = new System.Data.DataTable();
+            string qry = "SELECT * FROM ibis_pw_userlist WHERE ibis_pw_userlist_active = 1 AND ibis_pw_userlist_id > 1";
+            //string qry = "SELECT * FROM ibis_pw_userlist WHERE ibis_pw_userlist_active = 1";
+
+            Random randnum = new Random();
+            int _min4 = 0;
+            int _max4 = 9999;
+            int _min6 = 0;
+            int _max6 = 999999;
+
+            using (MySqlConnection dbh = new MySqlConnection(Properties.Resources.DB_CONNSTR_HES))
+            {
+                dbh.Open();
+                using (MySqlDataAdapter da = new MySqlDataAdapter(qry, dbh))
+                {
+                    da.Fill(dt);
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        doc = app.Documents.Add(@"C:\Users\dc1_000\Documents\HoldenEngineering\pw_memo_template.docx");
+                        doc.Activate();
+
+                        string newpw;
+                        if (row["ibis_pw_userlist_un"].ToString().Equals("dholden"))
+                        {
+                            newpw = String.Format("{0:000000}", randnum.Next(_min6, _max6));
+                        }
+                        else
+                        {
+                            // HERE IS WHERE ONE COULD RANDOMIZE THE PLACEMENT OF
+                            // THE ABBREVIATION RELATIVE TO THE FOUR-DIGIT NUMBER
+
+                            newpw = row["ibis_pw_userlist_abbr"].ToString() + String.Format("{0:0000}", randnum.Next(_min4, _max4));
+                        }
+
+                        doc.Bookmarks["bmNameLbl"].Range.Text = row["ibis_pw_userlist_label"].ToString();
+                        doc.Bookmarks["bmGenPW"].Range.Text = newpw;
+                        doc.Bookmarks["bmUserName"].Range.Text = row["ibis_pw_userlist_un"].ToString();
+                        doc.Bookmarks["bmOldPW"].Range.Text = row["ibis_pw_userlist_currpw"].ToString();
+                        doc.Bookmarks["bmNewPW"].Range.Text = newpw;
+                        doc.Bookmarks["bmNewPW2"].Range.Text = newpw;
+
+                        doc.PrintOut();
+                        ((Microsoft.Office.Interop.Word._Document)doc).Close(false, ref missing, ref missing);
+
+                        try
+                        {
+                            MySqlCommand cmd = new MySqlCommand();
+                            cmd.Connection = dbh;
+
+                            cmd.CommandText = "UPDATE ibis_pw_userlist SET ibis_pw_userlist_currpw = @npw, ibis_pw_userlist_prevpw = @opw WHERE ibis_pw_userlist_id = @uid";
+                            cmd.Prepare();
+
+                            cmd.Parameters.AddWithValue("@npw", newpw);
+                            cmd.Parameters.AddWithValue("@opw", row["ibis_pw_userlist_currpw"].ToString());
+                            cmd.Parameters.AddWithValue("@uid", row["ibis_pw_userlist_id"].ToString());
+
+                            cmd.ExecuteNonQuery();
+                        }
+                        catch (MySqlException mysqle)
+                        {
+                            MessageBox.Show("mysql ERROR: " + mysqle.ToString());
+                            return;
+                        }
+                        catch (Exception m1ex)
+                        {
+                            MessageBox.Show("M1_ERROR: " + m1ex.ToString());
+                            return;
+                        }
+                    }
+                }
+                dbh.Close();
+            }
+        }
+
+        private void btnCloseAndExit_Click(object sender, EventArgs e)
+        {
+            ((Microsoft.Office.Interop.Word._Application)app).Quit(false, ref missing, ref missing);
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnPrintPWMemos_Click(object sender, EventArgs e)
+        {
+            app = new Microsoft.Office.Interop.Word.Application { Visible = false };
+
+            System.Data.DataTable dt = new System.Data.DataTable();
+            string qry = "SELECT * FROM ibis_pw_userlist WHERE ibis_pw_userlist_active = 1";
+
+            using (MySqlConnection dbh = new MySqlConnection(Properties.Resources.DB_CONNSTR_HES))
+            {
+                dbh.Open();
+                using (MySqlDataAdapter da = new MySqlDataAdapter(qry, dbh))
+                {
+                    da.Fill(dt);
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        doc = app.Documents.Add(@"C:\Users\dc1_000\Documents\HoldenEngineering\pw_memo_template.docx");
+                        doc.Activate();
+
+                        doc.Bookmarks["bmNameLbl"].Range.Text = row["ibis_pw_userlist_label"].ToString();
+                        doc.Bookmarks["bmGenPW"].Range.Text = row["ibis_pw_userlist_currpw"].ToString();
+                        doc.Bookmarks["bmUserName"].Range.Text = row["ibis_pw_userlist_un"].ToString();
+                        doc.Bookmarks["bmOldPW"].Range.Text = row["ibis_pw_userlist_prevpw"].ToString();
+                        doc.Bookmarks["bmNewPW"].Range.Text = row["ibis_pw_userlist_currpw"].ToString();
+                        doc.Bookmarks["bmNewPW2"].Range.Text = row["ibis_pw_userlist_currpw"].ToString();
+
+                        doc.PrintOut();
+                        ((Microsoft.Office.Interop.Word._Document)doc).Close(false, ref missing, ref missing);
+                    }
+                }
+                dbh.Close();
+            }
+
+        }
+    }
+}
